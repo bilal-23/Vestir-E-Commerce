62967fadbd4cb1382de7034cfa60f4960143cda2
Bilal <mansuribilal101@gmail.com>
2023-05-27 18:55:12 +0530

    :zap: Feat: Login Api

---
+++ backend/src/pages/api/auth/login.ts
@@ -0,0 +1,71 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+    // Check if it is a GET request
+    if (req.method !== 'POST') {
+        return res.status(405).json({ message: 'Method not allowed' });
+    }
+
+    // get the user email and password from the request body
+    const { email, password } = req.body;
+
+    // Check if the email and password are valid
+    if (!email || !password) {
+        return res.status(400).json({ message: 'Bad request' });
+    }
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailRegex.test(email)) {
+        return res.status(400).json({ message: 'Bad request', error: 'Invalid email' });
+    }
+    if (password.length < 6) {
+        return res.status(400).json({ message: 'Bad request', error: 'Password should be at least 6 characters in length' });
+    }
+
+    // Connect to the mongodb clinet using mongoose database and throw error response if failed
+    const mongoose = require('mongoose');
+    let client = null;
+    try {
+        client = await mongoose.connect(process.env.MONGODB_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+    }
+    catch (error) {
+        return res.status(500).json({ message: "Cannot connect to the database" });
+    }
+
+    // Get the users collection or throw error response if failed
+    try {
+        const db = client.connection.db;
+        const users = db.collection('users');
+
+        // Check if  user already exists
+        const user = await users.findOne({ email: email });
+        // User exists, check if the password is correct
+        if (user) {
+            // Check if the password is correct
+            if (user.password !== password) {
+                // Close the database connection
+                client.connection.close();
+                return res.status(400).json({ message: "Incorrect password" });
+            }
+            // Password is correct, get a jwt token and store the email in the token and _id in the cookie
+            const jwt = require('jsonwebtoken');
+            const token = jwt.sign({ email: email, _id: user._id }, process.env.JWT_SECRET, { expiresIn: '1h' });
+            client.connection.close();
+            return res.status(200).json({ message: "Login successfully", token: token });
+        }
+        else {
+            // if user does not exists, cannot log in
+            // Close the database connection
+            client.connection.close();
+            return res.status(400).json({ message: "User does not exist" });
+        }
+    }
+
+    catch (error) {
+        // Close the database connection
+        client.connection.close();
+        return res.status(500).json({ message: "Cannot register account, please try again later." });
+    }
+}
